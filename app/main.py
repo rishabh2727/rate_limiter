32fd172ce9bd7d73105d1911ae287a5631d6ff5d
Rishabh Mehra <rishabhmehra2710@gmail.com>
2026-02-16 15:44:49 -0700

    setting up docker, storing redis instance inside

--- app/main.py
+++ app/main.py
@@ -1,2 +1,10 @@
-from fastapi import FastApi,Request
+from fastapi import FastAPI,Request, HttpException
 from app.limiter import rate_limiter
+import redis.asyncio as redis
+import time
+
+app = FastAPI()
+
+
+r = redis.from_url("redis://localhost:6379", decode_response = "True")
+
